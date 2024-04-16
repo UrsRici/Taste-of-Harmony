e813a26ef63d10bdcabcbce056594da2ed84073a
UrsRici <ursrici.ru@gmail.com>
2024-04-17 02:12:11 +0300

    admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -6,3 +6,34 @@ if (localStorage.getItem('user')) {
 } else {
     window.location.href = "Home.html";
 }
+
+function admin_title(id)
+{
+    let cls = id.slice(0, -1);
+    //eleiminare atribute active
+    for (let i = 1; i < 5; i++) 
+    {
+        document.getElementById(cls+i).removeAttribute('class');
+        document.getElementById(cls+i+'_table').setAttribute('hidden', true);
+        catehories_title('categories_title_0');
+    }
+    //Activare item selectat
+    document.getElementById(id).setAttribute('class', 'active');
+    document.getElementById(id + '_table').removeAttribute('hidden');
+}
+
+function catehories_title(id)
+{
+    let cls = id.slice(0, -1);
+    //eleiminare atribute active
+    for (let i = 1; i < 10; i++) 
+    {
+        document.getElementById(cls+i).removeAttribute('class');
+        document.getElementById(cls+i+'_table').setAttribute('hidden', true);
+    }
+    //Activare item selectat
+    if (id != 'categories_title_0') {
+        document.getElementById(id).setAttribute('class', 'active');
+        document.getElementById(id + '_table').removeAttribute('hidden');
+    }
+}
